15e218dbefb231d19719dd5ddbd8cd22b24ca0eb
saikumar <saikumaruppugandla@gmail.com>
2024-06-16 10:48:23 -0700

    manual prompt

--- src/app/page.js
+++ src/app/page.js
@@ -7,21 +7,36 @@ import Box from "@mui/material/Box";
 export default function Home() {
 
 
+  
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/sw.js')
-        .then(() => {
+    const registerServiceWorker = async () => {
+      if ('serviceWorker' in navigator) {
+        try {
+          const registration = await navigator.serviceWorker.register('/sw.js');
           console.log('Service Worker registered successfully');
-        })
-        .catch((error) => {
+
+          // Initialize deferredPrompt for later use to show browser install prompt
+          let deferredPrompt;
+
+          // Add an event listener for 'beforeinstallprompt' event
+          window.addEventListener('beforeinstallprompt', (e) => {
+            // Prevent the mini-infobar from appearing
+            e.preventDefault();
+            // Stash the event so it can be triggered later
+            deferredPrompt = e;
+            // Show the install prompt to the user
+            deferredPrompt.prompt();
+            // Optionally, send analytics event that PWA install promo was shown
+            console.log(`'beforeinstallprompt' event was fired.`);
+          });
+        } catch (error) {
           console.error('Service Worker registration failed:', error);
-        });
-    }
+        }
+      }
+    };
 
-    
+    registerServiceWorker();
   }, []);
-
 
  
   return (
