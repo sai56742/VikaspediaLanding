dbf47d259c321ce7e3b725175312e83fab6dbf22
saikumar <saikumaruppugandla@gmail.com>
2024-06-17 12:29:51 -0700

    fetch data

--- public/sw.js
+++ public/sw.js
@@ -12,14 +12,14 @@ self.addEventListener("install", function (event) {
         "/_next/static/css/app/page.css",
         "/_next/static/css/app/layout.css",
         "/_next/static/chunks/app-pages-internals.js",
-        // "/appstore/vikaspedialogos/newLogo.png",
-        // "/appstore/vikaspedialogos/vikaspediaLogo.png",
-        // "/appstore/appstore/200x75.png",
-        // "/appstore/playstore/200x75.png",
-        // "/appstore/vikaspedialogos/LandingPageBg.jpeg",
-        // "/officialslogo/cdac/75x75.png",
-        // "/officialslogo/digitalindia/200x70.png",
-        // "/officialslogo/meity/200x75.png",
+        "/appstore/vikaspedialogos/newLogo.png",
+        "/appstore/vikaspedialogos/vikaspediaLogo.png",
+        "/appstore/appstore/200x75.png",
+        "/appstore/playstore/200x75.png",
+        "/appstore/vikaspedialogos/LandingPageBg.jpeg",
+        "/officialslogo/cdac/75x75.png",
+        "/officialslogo/digitalindia/200x70.png",
+        "/officialslogo/meity/200x75.png",
         // "https://static.vikaspedia.in/media/pwa/logo144.png",
         // "https://vikaspedia.in/appstore/vikaspedialogos/newLogo.png",
       ]);
@@ -32,26 +32,40 @@ self.addEventListener("activate", function (event) {
   return self.clients.claim();
 });
 
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", (event) => {
+  // Cache-First Strategy
   event.respondWith(
     caches
-      .match(event.request)
-      .then(function (response) {
-        if (response) {
-          return response;
-        } else {
-          // return fetch(event.request)
-          //   .then(function (res) {
-          //     return caches.open(CACHE_DYNAMIC).then(function (cache) {
-          //       cache.put(event.request.url, res.clone());
-          //       return res;
-          //     });
-          //   })
-          //   .catch(function (err) {});
-        }
-      })
-      .catch((err) => {
-        console.log("check the err===>", err);
-      })
+      .match(event.request) // check if the request has already been cached
+      .then((cached) => cached || fetch(event.request)) // otherwise request network
+    // .then(
+    //   (response) =>
+    //     cache(event.request, response) // put response in cache
+    //       .then(() => response) // resolve promise with the network response
+    // )
   );
 });
+
+// self.addEventListener("fetch", function (event) {
+//   event.respondWith(
+//     caches
+//       .match(event.request)
+//       .then(function (response) {
+//         if (response) {
+//           return response || event.request.url;
+//         } else {
+//           // return fetch(event.request)
+//           //   .then(function (res) {
+//           //     return caches.open(CACHE_DYNAMIC).then(function (cache) {
+//           //       cache.put(event.request.url, res.clone());
+//           //       return res;
+//           //     });
+//           //   })
+//           //   .catch(function (err) {});
+//         }
+//       })
+//       .catch((err) => {
+//         console.log("check the err===>");
+//       })
+//   );
+// });
